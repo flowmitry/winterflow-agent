ea9b301cfcbd1a66c62b5e64618191bba0239820
Dmitry <1319595+flowmitry@users.noreply.github.com>
2025-05-15 20:26:23 -0700

    Add certificate handling to agent registration process

--- pkg/certs/certs.go
+++ pkg/certs/certs.go
@@ -1,140 +1,140 @@
 package certs
 
 import (
-    "bytes"
-    "crypto/rand"
-    "crypto/rsa"
-    "crypto/x509"
-    "crypto/x509/pkix"
-    "encoding/pem"
-    "fmt"
-    "os"
-    "path/filepath"
-    log "winterflow-agent/pkg/log"
+	"bytes"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"fmt"
+	"os"
+	"path/filepath"
+	log "winterflow-agent/pkg/log"
 )
 
 // GeneratePrivateKey generates a new RSA private key and saves it to the specified path
 func GeneratePrivateKey(keyPath string) error {
-    // Create directory if it doesn't exist
-    dir := filepath.Dir(keyPath)
-    if err := os.MkdirAll(dir, 0755); err != nil {
-        return fmt.Errorf("failed to create directory for private key: %v", err)
-    }
+	// Create directory if it doesn't exist
+	dir := filepath.Dir(keyPath)
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return fmt.Errorf("failed to create directory for private key: %v", err)
+	}
 
-    // Generate private key
-    privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
-    if err != nil {
-        return fmt.Errorf("failed to generate private key: %v", err)
-    }
+	// Generate private key
+	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		return fmt.Errorf("failed to generate private key: %v", err)
+	}
 
-    // Convert to PEM format
-    privateKeyPEM := &pem.Block{
-        Type:  "RSA PRIVATE KEY",
-        Bytes: x509.MarshalPKCS1PrivateKey(privateKey),
-    }
+	// Convert to PEM format
+	privateKeyPEM := &pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: x509.MarshalPKCS1PrivateKey(privateKey),
+	}
 
-    // Create file
-    keyFile, err := os.OpenFile(keyPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
-    if err != nil {
-        return fmt.Errorf("failed to create private key file: %v", err)
-    }
-    defer keyFile.Close()
+	// Create file
+	keyFile, err := os.OpenFile(keyPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		return fmt.Errorf("failed to create private key file: %v", err)
+	}
+	defer keyFile.Close()
 
-    // Write PEM to file
-    if err := pem.Encode(keyFile, privateKeyPEM); err != nil {
-        return fmt.Errorf("failed to write private key to file: %v", err)
-    }
+	// Write PEM to file
+	if err := pem.Encode(keyFile, privateKeyPEM); err != nil {
+		return fmt.Errorf("failed to write private key to file: %v", err)
+	}
 
-    log.Printf("[DEBUG] Generated private key at: %s", keyPath)
-    return nil
+	log.Printf("[DEBUG] Generated private key at: %s", keyPath)
+	return nil
 }
 
 // SaveCertificate saves the certificate data to the specified path
 func SaveCertificate(certData, certPath string) error {
-    // Create directory if it doesn't exist
-    dir := filepath.Dir(certPath)
-    if err := os.MkdirAll(dir, 0755); err != nil {
-        return fmt.Errorf("failed to create directory for certificate: %v", err)
-    }
+	// Create directory if it doesn't exist
+	dir := filepath.Dir(certPath)
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return fmt.Errorf("failed to create directory for certificate: %v", err)
+	}
 
-    // Create file
-    certFile, err := os.OpenFile(certPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
-    if err != nil {
-        return fmt.Errorf("failed to create certificate file: %v", err)
-    }
-    defer certFile.Close()
+	// Create file
+	certFile, err := os.OpenFile(certPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		return fmt.Errorf("failed to create certificate file: %v", err)
+	}
+	defer certFile.Close()
 
-    // Write certificate data to file
-    if _, err := certFile.WriteString(certData); err != nil {
-        return fmt.Errorf("failed to write certificate to file: %v", err)
-    }
+	// Write certificate data to file
+	if _, err := certFile.WriteString(certData); err != nil {
+		return fmt.Errorf("failed to write certificate to file: %v", err)
+	}
 
-    log.Printf("[DEBUG] Saved certificate at: %s", certPath)
-    return nil
+	log.Printf("[DEBUG] Saved certificate at: %s", certPath)
+	return nil
 }
 
 // CreateCSR creates a Certificate Signing Request with the given private key and saves it to the specified path
 func CreateCSR(certificateID string, privateKeyPath, csrPath string) (string, error) {
-    // Create directory if it doesn't exist
-    dir := filepath.Dir(csrPath)
-    if err := os.MkdirAll(dir, 0755); err != nil {
-        return "", fmt.Errorf("failed to create directory for CSR: %v", err)
-    }
+	// Create directory if it doesn't exist
+	dir := filepath.Dir(csrPath)
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return "", fmt.Errorf("failed to create directory for CSR: %v", err)
+	}
 
-    // Read private key
-    keyData, err := os.ReadFile(privateKeyPath)
-    if err != nil {
-        return "", fmt.Errorf("failed to read private key: %v", err)
-    }
+	// Read private key
+	keyData, err := os.ReadFile(privateKeyPath)
+	if err != nil {
+		return "", fmt.Errorf("failed to read private key: %v", err)
+	}
 
-    // Parse private key
-    block, _ := pem.Decode(keyData)
-    if block == nil {
-        return "", fmt.Errorf("failed to parse private key PEM")
-    }
+	// Parse private key
+	block, _ := pem.Decode(keyData)
+	if block == nil {
+		return "", fmt.Errorf("failed to parse private key PEM")
+	}
 
-    privateKey, err := x509.ParsePKCS1PrivateKey(block.Bytes)
-    if err != nil {
-        return "", fmt.Errorf("failed to parse private key: %v", err)
-    }
+	privateKey, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+	if err != nil {
+		return "", fmt.Errorf("failed to parse private key: %v", err)
+	}
 
-    // Create CSR template
-    template := x509.CertificateRequest{
-        Subject: pkix.Name{
-            CommonName: certificateID,
-        },
-    }
+	// Create CSR template
+	template := x509.CertificateRequest{
+		Subject: pkix.Name{
+			CommonName: certificateID,
+		},
+	}
 
-    // Create CSR
-    csrBytes, err := x509.CreateCertificateRequest(rand.Reader, &template, privateKey)
-    if err != nil {
-        return "", fmt.Errorf("failed to create CSR: %v", err)
-    }
+	// Create CSR
+	csrBytes, err := x509.CreateCertificateRequest(rand.Reader, &template, privateKey)
+	if err != nil {
+		return "", fmt.Errorf("failed to create CSR: %v", err)
+	}
 
-    // Convert to PEM format
-    csrPEM := &pem.Block{
-        Type:  "CERTIFICATE REQUEST",
-        Bytes: csrBytes,
-    }
+	// Convert to PEM format
+	csrPEM := &pem.Block{
+		Type:  "CERTIFICATE REQUEST",
+		Bytes: csrBytes,
+	}
 
-    // Create file
-    csrFile, err := os.OpenFile(csrPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
-    if err != nil {
-        return "", fmt.Errorf("failed to create CSR file: %v", err)
-    }
-    defer csrFile.Close()
+	// Create file
+	csrFile, err := os.OpenFile(csrPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		return "", fmt.Errorf("failed to create CSR file: %v", err)
+	}
+	defer csrFile.Close()
 
-    // Write PEM to file
-    if err := pem.Encode(csrFile, csrPEM); err != nil {
-        return "", fmt.Errorf("failed to write CSR to file: %v", err)
-    }
+	// Write PEM to file
+	if err := pem.Encode(csrFile, csrPEM); err != nil {
+		return "", fmt.Errorf("failed to write CSR to file: %v", err)
+	}
 
-    // Convert CSR to string for API submission
-    var csrBuffer bytes.Buffer
-    if err := pem.Encode(&csrBuffer, csrPEM); err != nil {
-        return "", fmt.Errorf("failed to encode CSR to string: %v", err)
-    }
+	// Convert CSR to string for API submission
+	var csrBuffer bytes.Buffer
+	if err := pem.Encode(&csrBuffer, csrPEM); err != nil {
+		return "", fmt.Errorf("failed to encode CSR to string: %v", err)
+	}
 
-    log.Printf("[DEBUG] Created CSR at: %s with Common Name: %s", csrPath, certificateID)
-    return csrBuffer.String(), nil
+	log.Printf("[DEBUG] Created CSR at: %s with Common Name: %s", csrPath, certificateID)
+	return csrBuffer.String(), nil
 }
