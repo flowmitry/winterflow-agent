6b3f36e6623ca7b34f7ac65884a62c56048a30bc
Dmitry <1319595+flowmitry@users.noreply.github.com>
2025-06-08 21:45:43 -0700

    Add agent startup playbook and agent version capability

---
+++ pkg/capabilities/agent_version.go
@@ -0,0 +1,27 @@
+package capabilities
+
+import (
+	"winterflow-agent/internal/version"
+)
+
+// AgentVersionCapability represents the Agent Version capability
+type AgentVersionCapability struct {
+	version string
+}
+
+// NewAgentVersionCapability creates a new Agent Version capability
+func NewAgentVersionCapability() *AgentVersionCapability {
+	return &AgentVersionCapability{
+		version: version.GetVersion(),
+	}
+}
+
+// Name returns the name of the capability
+func (c *AgentVersionCapability) Name() string {
+	return CapabilityAgentVersion
+}
+
+// Value returns the value of the capability
+func (c *AgentVersionCapability) Value() string {
+	return c.version
+}
